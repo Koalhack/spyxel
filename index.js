f515fe75d90fe0f8ef924f781e8c493f12d90b31
KoalHack <alexandre.saner@gmail.com>
2023-09-24 18:42:09 +0200

    style(index): add comment to all index.js file for better comprehension

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const pixelPath = path.join(__dirname, '/pixel.png');
 const app = express();
 const port = 8080;
 
+//NOTE: Format the time (Y-m-d H:M:S)
 function formatTime(time) {
   const format = {
     day: time.getDate(),
@@ -24,6 +25,7 @@ function formatTime(time) {
   return `${format.year}-${format.month}-${format.day} ${format.hours}:${format.minutes}:${format.seconds}`;
 }
 
+//NOTE: Get IP from user
 function getIpFromRequest(req) {
   let ips = (
     req.headers['cf-connecting-ip'] ||
@@ -36,16 +38,20 @@ function getIpFromRequest(req) {
   return ips[0].trim();
 }
 
+//NOTE: Get IP address location with geolocation-db
 async function locateIpAddress(Address) {
   const response = await fetch(`https://geolocation-db.com/json/${Address}`);
   return await response.json();
 }
 
+//NOTE: Route for the default page
 app.get('/', async (req, res) => {
+  //NOTE: Send pixel image to route
   res.sendFile(pixelPath);
 
   const currentTime = new Date();
   const timeStamp = formatTime(currentTime);
+
   const userAgent = req.header('User-Agent');
   const userIp = getIpFromRequest(req);
   const countryName = (await locateIpAddress(userIp))?.country_name;
@@ -56,6 +62,7 @@ app.get('/', async (req, res) => {
   console.log(countryName);
 });
 
+//NOTE: Launch app to specified port
 app.listen(port, () => {
   console.log(`server listening on port : ${port}`);
 });
