04fb3c5625df1e0ff5c96f063a99c8f771d3e960
KoalHack <alexandre.saner@gmail.com>
2023-09-25 15:35:20 +0200

    fix(index): fix timestamp numbers display correct number of digit

--- index.js
+++ index.js
@@ -16,12 +16,12 @@ const port = 8080;
 //NOTE: Format the time (Y-m-d H:M:S)
 function formatTime(time) {
   const format = {
-    day: time.getDate(),
-    month: time.getMonth() + 1,
-    year: time.getFullYear(),
-    hours: time.getHours(),
-    minutes: time.getMinutes(),
-    seconds: time.getSeconds()
+    day: String(time.getDate()).padStart(2, '0'),
+    month: String(time.getMonth() + 1).padStart(2, '0'),
+    year: String(time.getFullYear()).padStart(4, '0'),
+    hours: String(time.getHours()).padStart(2, '0'),
+    minutes: String(time.getMinutes()).padStart(2, '0'),
+    seconds: String(time.getSeconds()).padStart(2, '0')
   };
 
   return `${format.year}-${format.month}-${format.day} ${format.hours}:${format.minutes}:${format.seconds}`;
